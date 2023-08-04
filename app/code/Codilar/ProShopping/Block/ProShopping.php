16eef73b2ca3a56521d05723151e9127cc31f530
sanjay <sanjay.d@codilar.com>
2023-08-05 01:06:45 +0530

    added some basic pop-up modal

---
+++ app/code/Codilar/ProShopping/Block/ProShopping.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Codilar\ProShopping\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class ProShopping extends Template
+{
+    public function getContent() : string
+    {
+        return 'Pro Shopping Recommendation Popup';
+    }
+}
