32c164e1183889900eb0a2024e7d6aa9d8b51f66
itzlehanCodilar <lehanmax.d@codilar.com>
2023-08-05 20:16:13 +0530

    created new theme for pro active theme

---
+++ app/design/frontend/Codilar/pro-active-shop/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Codilar/pro-active-shop',
+    __DIR__
+);
