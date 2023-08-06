7bf106476f5955496c3cf13d4c12224b3482ec5a
@Sachin1830 <sachin.b@codilar.com>
2023-08-06 08:29:33 +0530

    changes in the product by category id

---
+++ app/code/Codilar/ProShopping/Controller/Recommend/ProductListByCategory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Codilar\ProShopping\Controller\Recommend;
+
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Get product by category id
+ */
+class ProductListByCategory implements HttpPostActionInterface
+{
+
+    public function __construct(
+        private RequestInterface $request,
+        private JsonFactory $jsonFactory,
+        private CollectionFactory $collectionFactory,
+        private StoreManagerInterface $storeManager
+    ) {
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * @return ResultInterface|ResponseInterface
+     * @throws NotFoundException
+     */
+    public function execute()
+    {
+        try {
+            $baseUrl = $this->storeManager->getStore()?->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA)
+                . 'catalog/product';
+        } catch (NoSuchEntityException $e) {
+            $baseUrl = null;
+        }
+
+        $id = $this->request->getParam('categoryId');
+        $productCollection = $this->collectionFactory->create();
+        $productCollection->addAttributeToSelect('*');
+        $productCollection->addCategoriesFilter(['eq' => $id]);
+        $products = $productCollection->getItems();
+        $productArr = [];
+        foreach ($products as $product) {
+            $productArr[] = [
+                'id' => $product->getId(),
+                "sku" => $product->getSku(),
+                "image" => $baseUrl . $product->getImage(),
+                "price" => $product->getPrice()
+            ];
+        }
+        $jsonResult = json_encode($productArr);
+        $result = $this->jsonFactory->create();
+        $result->setData(['output' => $jsonResult]);
+        return $result;
+    }
+}
